1a038123f6b2442cb6c5bae1c50ca1ebce7fcc25
Kiran Duggirla <kduggirala@gvcgroup.com>
2019-11-25 20:04:53 +0530

    Initial commit

---
+++ Controllers/ContentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ContentApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContentController : ControllerBase
+    {
+        private readonly ILogger<ContentController> _logger;
+
+        public ContentController(ILogger<ContentController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Object Get()
+        {
+            return "test";
+            //Write your code here
+        }
+    }
+}
